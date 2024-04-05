d030d2b912a225135c5ebb67802a52624da79a5f
bharathiraja-developer <bharathiraja798@gmail.com>
2024-04-05 11:58:25 +0530

    Backend

--- models/bookings.js
+++ models/bookings.js
@@ -2,26 +2,7 @@ const moongose = require("mongoose");
 
 const bookingSchema = new moongose.Schema({
   username: String,
-  details: [
-    {
-      modelName: String,
-      price: Array,
-      location: String,
-      pick: String,
-      drop: String,
-      image: String,
-      bookedAt: String,
-      details: {
-        kilometers: String,
-        selfstart: Boolean,
-        cc: String,
-        year: Number,
-        brand: String,
-        fuel: String,
-        Available: Number,
-      },
-    },
-  ],
+  details: Array,
 });
 
 module.exports = moongose.model("booking", bookingSchema, "bookings");
