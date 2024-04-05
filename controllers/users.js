02b88a37d88e7d75e3c2549a79be72bf13de56bd
bharathiraja-developer <bharathiraja798@gmail.com>
2024-04-05 11:23:02 +0530

    Backend

--- controllers/users.js
+++ controllers/users.js
@@ -148,12 +148,10 @@ const userController = {
         },
       ];
       const bikeBook = await bikes.findByIdAndUpdate(id, { details: Details });
-      const booking = await bookings.replaceOne(
-        { username },
-        {
-          details: details,
-        }
-      );
+      const booking = await bookings.findOneAndReplace(book, {
+        username,
+        details,
+      });
       response.status(200).json({ message: "Bike booked successfully" });
     } catch (error) {
       response.status(500).json({ error: error.message });
